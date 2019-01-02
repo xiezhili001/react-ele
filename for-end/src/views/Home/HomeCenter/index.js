48ccfdd1c1af87740e0f95e9215d1eca5ec37fba
zhimo <www.1020929225@qq.com>
2019-01-02 15:46:59 +0800

    update Bash

--- for-end/src/views/Home/HomeCenter/index.js
+++ for-end/src/views/Home/HomeCenter/index.js
@@ -1,9 +1,64 @@
 import React from 'react';
+import './index.scss';
 
 class HomeCenter extends React.Component {
   render() {
     return (
-      <div>个人中心</div>
+      <div className="center">
+        <div className="profile-1vcJW">
+          <section>
+            <div className="profile-3g0uv">
+              <div className="index-18Ili">
+                <span></span>
+              </div>
+              <div className="profile-xfCcC">
+                <p className="profile-1_mtk">
+                  <span>44899d5d1</span>
+                </p>
+                <p className="profile-1UP72">
+                  <i className="iconfont icon-shouji"></i>
+                  <span>183****0481</span>
+                </p>
+              </div>
+              <span className="profile-2XuMq">
+                <i className="iconfont icon-xiangyou"></i>
+              </span>
+            </div>
+          </section>
+          <div data-spm="maininfo" className="index-1G7HV">
+            <a href="javascript:;" className="index-1ryAh">
+              <p>
+                <span className="index-2FmrF">0.00</span>
+                <span className="index-2V-Hh">元</span>
+              </p>
+              <p className="index-3S6cZ">钱包</p>
+            </a>
+            <a href="javascript:;" className="index-1ryAh">
+              <p>
+                <span className="index-2FmrF red">1</span>
+                <span className="index-2V-Hh red">个</span>
+              </p>
+              <p className="index-3S6cZ">红包</p>
+            </a>
+            <a href="javascript:;" className="index-1ryAh">
+              <p>
+                <span className="index-2FmrF green">0</span>
+                <span className="index-2V-Hh green">个</span>
+              </p>
+              <p className="index-3S6cZ">金币</p>
+            </a>
+          </div>
+          <section class="profile-1reTe">
+            <a href="javascript:;" class="index-2MEEn">
+              <div class="index-yydpu">
+              我的地址
+                <span class="index-3Z8It">
+                </span>
+              </div>
+            </a>
+          </section>
+        </div>
+      </div>
     )
   }
 }
