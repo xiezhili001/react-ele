34efdbf07c017b3b2a7e557210ff9dec1fb1c965
xiezhili <xiezhili001@163.com>
2019-01-03 09:35:57 +0800

    detailCity back-end

---
+++ back-end/server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+
+// 引入路由模块
+var FilmRouter = require('./routes/film.js');
+var UserRouter = require('./routes/user.js');
+var DetailRouter = require('./routes/detail.js');
+var LoginRouter =  require('./routes/login.js')
+
+var DetailCity =  require('./routes/detailCity.js')
+// 使用路由模块，中间件
+app.use('/api/film/', FilmRouter);
+app.use('/api/user/', UserRouter);
+app.use('/api/detail/', DetailRouter);
+app.use('/api/login/', LoginRouter);
+
+app.use('/api/detailCity/', DetailCity);
+
+
+
+app.listen(3000);
+console.log('服务启动成功');
