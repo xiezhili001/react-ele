c43311b9e55c5d40b29616f1a45bef91070176a9
zhimo <www.1020929225@qq.com>
2019-01-03 18:42:06 +0800

    判断是否登录，主页组件切换

--- for-end/src/views/Home/HomeCenter/index.js
+++ for-end/src/views/Home/HomeCenter/index.js
@@ -3,6 +3,24 @@ import { Link } from 'react-router-dom';
 import './index.scss';
 
 class HomeCenter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username:''
+    }
+  }
+  componentDidMount () {
+    if (localStorage.getItem("username") === null) {
+      this.refs["xxoo"].style.display = "none";
+      this.refs["aabb"].style.display = "none";
+      this.refs["abab"].style.display = "none";
+    }else {
+      this.refs["ooxx"].style.display = "none";
+      this.refs["bbaa"].style.display = "none";
+      this.refs["baba"].style.display = "none";
+    }
+  }
+
   render() {
     return (
       <div className="center">
@@ -14,11 +32,13 @@ class HomeCenter extends React.Component {
               </div>
               <div className="profile-xfCcC">
                 <p className="profile-1_mtk">
-                  <span>44899d5d1</span>
+                  <span ref="abab" >44899d5d1</span>
+                  <span ref="baba" >登录/注册</span>
                 </p>
                 <p className="profile-1UP72">
                   <i className="iconfont icon-shouji"></i>
-                  <span>183****0481</span>
+                  <span ref="aabb" >183****0481</span>
+                  <span ref="bbaa" >登录后享受更多特权</span>
                 </p>
               </div>
               <span className="profile-2XuMq">
@@ -26,7 +46,38 @@ class HomeCenter extends React.Component {
               </span>
             </div>
           </section>
-          <div data-spm="maininfo" className="index-1G7HV">
+          {/* 这里写未登录时需要引入的组件 */}
+          <div data-spm="maininfo" className="index-1G7HV"  ref="ooxx" >
+            <div className="index-1ryAh">
+              <p>
+                <span className="index-2FmrF">
+                  <i className="iconfont icon-renminbi2"></i>
+                </span>
+                {/* <span className="index-2V-Hh">元</span> */}
+              </p>
+              <p className="index-3S6cZ">钱包</p>
+            </div>
+            <div className="index-1ryAh">
+              <p>
+                <span className="index-2FmrF red">
+                <i className="iconfont icon-hongbao"></i>
+                </span>
+                {/* <span className="index-2V-Hh red">个</span> */}
+              </p>
+              <p className="index-3S6cZ">红包</p>
+            </div>
+            <div className="index-1ryAh">
+              <p>
+                <span className="index-2FmrF green">
+                <i className="iconfont icon-qian2"></i>
+                </span>
+                {/* <span className="index-2V-Hh green">个</span> */}
+              </p>
+              <p className="index-3S6cZ">金币</p>
+            </div>
+          </div>
+          {/* --------------------------------------- */}
+          <div data-spm="maininfo" className="index-1G7HV" ref="xxoo" >
             <div className="index-1ryAh">
               <p>
                 <span className="index-2FmrF">0.00</span>
