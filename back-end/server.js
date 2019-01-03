100ecc0b34209968b81c23c430cb9e1dc6e1e036
xiezhili <xiezhili001@163.com>
2019-01-03 12:01:10 +0800

    user back

--- back-end/server.js
+++ back-end/server.js
@@ -3,20 +3,21 @@ var app = express();
 
 // 引入路由模块
 var FilmRouter = require('./routes/film.js');
-var UserRouter = require('./routes/user.js');
 var DetailRouter = require('./routes/detail.js');
 var LoginRouter =  require('./routes/login.js')
 
 var DetailCity =  require('./routes/detailCity.js')
 var HomePageGood =  require('./routes/homePageGood.js')
+var User =  require('./routes/user.js')
+
 // 使用路由模块，中间件
 app.use('/api/film/', FilmRouter);
-app.use('/api/user/', UserRouter);
 app.use('/api/detail/', DetailRouter);
 app.use('/api/login/', LoginRouter);
 
 app.use('/api/detailCity/', DetailCity);
 app.use('/api/homePageGood/', HomePageGood);
+app.use('/api/user/', User);
 
 
 
