72bbd6d5b587175ddd4f89b67b42ae28b9c008e3
xiezhili <xiezhili001@163.com>
2019-01-05 17:06:34 +0800

    哎

--- for-end/src/views/Home/HomeCard/index.js
+++ for-end/src/views/Home/HomeCard/index.js
@@ -1,38 +1,7 @@
 import React from 'react';
 import './index.scss';
+import Submit from "./Submit/index"
 
-const BeforeOrder = function () {
-  return <>
-    <div className="gao-order">
-      <div className="gao-order-main">
-        <div className="gao-order-main-top">
-          <a href="11">
-            <div className="top-left">
-              <div className="top">
-                {/* eslint-disable-next-line */}
-                <img src="https://fuss10.elemecdn.com/7/d3/48a777a6b444dc317cc24d101220cjpeg.jpeg?imageMogr/format/webp/thumbnail/!64x64r/gravity/Center/crop/64x64/"></img>
-              </div>
-              <div className="info">
-                <p className="p1">肯德基宅急送</p>
-                <p className="p2">10分钟前</p>
-              </div>
-            </div>
-            <div className="top-right">
-              <p>订单已取消</p>
-            </div>
-          </a>
-        </div>
-        <div className="gao-order-main-middle">
-          <p className="p1">新奥尔良烤鸡腿汉堡ST</p>
-          <p>￥28.50</p>
-        </div>
-        <div className="gao-order-main-bottom">
-          <p>再来一单</p>
-        </div>
-      </div>
-    </div>
-  </>
-}
 const AfterOrder = function () {
   return <>
     <div className="gao-order">
@@ -59,7 +28,7 @@ class HomeCard extends React.Component {
       )
     }else{
       return (
-        <BeforeOrder></BeforeOrder>
+        <Submit></Submit>
       )
     }
 
