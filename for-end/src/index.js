dd8761588b3c4bf59e9050aed372b6e15edcc5ea
xiezhili <xiezhili001@163.com>
2019-01-07 11:22:36 +0800

    end over

--- for-end/src/index.js
+++ for-end/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import axios from 'axios';
 import App from './App';
 import './styles/base.scss';
-
+axios.defaults.baseURL = 'https://hhhhh5.top:8080';
 ReactDOM.render(
     <App></App>,
   document.getElementById('root')
